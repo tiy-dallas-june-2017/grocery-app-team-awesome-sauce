21f3c9f232334323d32e2762bc1b0698d030248d
blaineCantrell <blaine.cantrell@gmail.com>
2017-08-03 15:00:26 -0500

    added comments to validator. Not hooked up.

--- validator.js
+++ validator.js
@@ -5,6 +5,8 @@ req.check('name', 'Name can not be empty').notEmpty(); //checking for empty name
 
 req.check('name', 'Your name can not be longer than 100 characters').isLength({ min: 0, max: 100}); //checking for character length
 
+// Still need error message for 0 quantity and price.
+
 req.getValidatorResult().then(function(result){
   if(result.isEmpty()){
     res.render('/error', req.body)
