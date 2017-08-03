7c13c267453388bb781b76814f062cda42bd6bff
blaineCantrell <blaine.cantrell@gmail.com>
2017-08-03 14:59:00 -0500

    added javascript file with validations. Not hooked up, needs to be assigned

---
+++ validator.js
@@ -0,0 +1,14 @@
+const expressValidator = require('express-validator');
+//These are the input errors for the 'Add an item' page.
+
+req.check('name', 'Name can not be empty').notEmpty(); //checking for empty name.
+
+req.check('name', 'Your name can not be longer than 100 characters').isLength({ min: 0, max: 100}); //checking for character length
+
+req.getValidatorResult().then(function(result){
+  if(result.isEmpty()){
+    res.render('/error', req.body)
+  } else {
+    res.render('/inventorydetail', data)
+  };
+});
