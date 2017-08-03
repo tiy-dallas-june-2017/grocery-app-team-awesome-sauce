15b135d75537941e7d7a2de58ea0907e7f892b1c
hagenwatkins <hd_watkins@yahoo.com>
2017-08-03 15:04:35 -0500

    add inventory

--- routes/inventory.js
+++ routes/inventory.js
@@ -13,24 +13,23 @@ router.get('/inventorydetail', function(req, res){
 });
 
 router.get('/inventory/add/:id', function(req, res){
-  inventoryModel.insert(req.params.id,function(err, result){
-
+  // inventoryModel.insert(req.params.id,function(err, result){
   res.render('inventorydetail');
   });
-});
 
 
-router.post('/inventory/add/:id',function(req, res){
-  inventoryModel.insert(req.params.id,function(err, result){
+
+router.post('/inventory/add',function(req, res){
+  // inventoryModel.insert(req.params.id,function(err, result){
     res.redirect('/inventory');
   });
 
-});
+
 router.post('/inventory/delete/:id',function(req,res){
-  inventoryModel.remove(req.params.id, function(err, result){
+  // inventoryModel.remove(req.params.id, function(err, result){
     res.redirect('/inventory');
   });
-});
+
 
 //   const db = mongo.db();
 //   db.collection('todos').find({}).toArray(function(err, data){
