482590e569fc7026ba94cc31d62ef8b8628b5a37
blaineCantrell <blaine.cantrell@gmail.com>
2017-08-03 09:54:55 -0500

    created public folder with index.html

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-//stuff n things 
+const express = require('express');
+const mustache = require('mustache-express');
+const bodyParse = require('body-parser');
+
+const app = express();
+app.use(bodyParse.urlencoded({ extended: false }));
+
+app.engine('mustache', mustache() );
+
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
+app.use(express.static('public'));
+
+app.listen(4040, function(){
+  console.log('listening to port 4040')
+});
