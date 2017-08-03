4482197c21734e0942a1ef516f9d77251d95f5f1
hagenwatkins <hd_watkins@yahoo.com>
2017-08-03 10:39:16 -0500

    update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mustache = require('mustache-express');
-<<<<<<< HEAD
+
 const bodyParse = require('body-parser');
 
 const app = express();
@@ -15,34 +15,3 @@ app.use(express.static('public'));
 
 app.listen(4040, function(){
   console.log('listening to port 4040')
-=======
-
-
-const bodyParser = require('body-parser')
-
-const app = express();
-app.use(bodyParser.urlencoded({extended:false}));
-
-
-
-const mustacheExpressInstance = mustache();
-mustacheExpressInstance.cache = null;
-
-app.engine('mustache', mustacheExpressInstance);
-
-app.set('view engine', 'mustache');
-app.set('views',__dirname + '/views');
-
-app.use(express.static('public'));
-
-app.use('/', employeesschedule);
-
-
-
-
-
-  app.listen(4444, function() {
-    console.log('Team awesome sauce');
-  });
->>>>>>> 1cdee419b2e51d51fc35389a4f91af776a64b4e2
-});
