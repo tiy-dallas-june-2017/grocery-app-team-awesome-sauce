186d1882e08cf2967004c98a0540d5f7531fb0f6
hagenwatkins <hd_watkins@yahoo.com>
2017-08-03 10:16:31 -0500

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-hi!
+const express = require('express');
+const mustache = require('mustache-express');
+
+
+const bodyParser = require('body-parser')
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:false}));
+
+
+
+const mustacheExpressInstance = mustache();
+mustacheExpressInstance.cache = null;
+
+app.engine('mustache', mustacheExpressInstance);
+
+app.set('view engine', 'mustache');
+app.set('views',__dirname + '/views');
+
+app.use(express.static('public'));
+
+
+
+
+
+  app.listen(4444, function() {
+    console.log('Team awesome sauce');
+  });
+});
