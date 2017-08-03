0f8c3a5393c09da069542f62117f004da54e9b45
blaineCantrell <blaine.cantrell@gmail.com>
2017-08-03 15:04:00 -0500

    merge changes

--- routes/schedule.js
+++ routes/schedule.js
@@ -4,13 +4,11 @@ router.get('/schedule', function(req, res) {
   res.render('employeeschedule');
 });
 
-<<<<<<< HEAD
 router.get('/schedule/editschedule', function(req, res){
   res.render('addSchedule')
-=======
+
 router.get('/addemployee',function(req, res) {
   res.render('addemployee');
->>>>>>> 0ec4df08bfc7e243f3aadf3f491ddbafdd264aae
 });
 
 module.exports = router;
