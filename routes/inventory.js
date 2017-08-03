74045dad10a66f9906131b593458b3b18b37dd5f
Jeff Irwin <jeff.irwin@live.com>
2017-08-03 18:21:41 -0500

    fixed merge conflicts in inventory.js

--- routes/inventory.js
+++ routes/inventory.js
@@ -19,6 +19,34 @@ router.get('/inventorydetail/:id', function(req, res){
   })
 });
 
+//posting and deleting to inventory database instance.
+router.post('/inventory',function(req, res){
+  inventoryModel.insert(req.body,function(err, result){
+    res.redirect('/inventory');
+  });
+});
+
+router.post('/delete/:id', function(req, res) {
+  inventoryModel.remove(req.params.id, function(err, result) {
+    res.render('inventorydetail');
+  });
+});
+
+router.get('/inventory/add/:id', function(req, res){
+  // inventoryModel.insert(req.params.id,function(err, result){
+  res.render('inventorydetail');
+});
+
+router.post('/inventory/add',function(req, res){
+  // inventoryModel.insert(req.params.id,function(err, result){
+    res.redirect('/inventory');
+});
+
+router.post('/inventory/delete/:id',function(req,res){
+  // inventoryModel.remove(req.params.id, function(err, result){
+    res.redirect('/inventory');
+});
+
 
 //   const db = mongo.db();
 //   db.collection('todos').find({}).toArray(function(err, data){
