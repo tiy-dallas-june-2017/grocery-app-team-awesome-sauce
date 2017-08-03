b66fb2f768d4619e8bed8691c7f357fc1d226fbc
hagenwatkins <hd_watkins@yahoo.com>
2017-08-03 14:50:05 -0500

    pulling

--- routes/inventory.js
+++ routes/inventory.js
@@ -11,8 +11,17 @@ router.get('/inventorydetail', function(req, res){
 
   res.render('inventorydetail');
 });
-router.post('/inventory',function(req, res){
-  inventoryModel.insert(req.body,function(err, result){
+
+router.get('/inventory/add/:id', function(req, res){
+  inventoryModel.insert(req.params.id,function(err, result){
+
+  res.render('inventorydetail');
+  });
+});
+
+
+router.post('/inventory/add/:id',function(req, res){
+  inventoryModel.insert(req.params.id,function(err, result){
     res.redirect('/inventory');
   });
 
