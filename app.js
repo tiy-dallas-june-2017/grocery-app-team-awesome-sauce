fbeb2f74a285cccb040bfcba4cd5b2d2ec0f36da
hagenwatkins <hd_watkins@yahoo.com>
2017-08-04 11:30:38 -0500

    some style

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use('/', inventory);
 //through inventory.js in routes
 
 //this now has my server name. --Stefanie
-let url = "mongodb://localhost:27017/inventorydb";
+let url = "mongodb://localhost:27017/inventory";
 mongo.connect(url, function(err, database){
   if(err) {
     console.log("error", err);
