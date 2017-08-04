811cf0ef192f290ffb456937e1da66ff9a6a83db
Jeff Irwin <jeff.irwin@live.com>
2017-08-04 11:57:05 -0500

    added employee edit page

--- routes/schedule.js
+++ routes/schedule.js
@@ -4,13 +4,16 @@ const bodyParser = require('body-parser');
 
 router.get('/schedule', function(req, res) {
   inventoryModel.getEmployeeSchedule(function(err, data) {
-    console.log(data);
     res.render('employeeschedule', { schedule: data });
   });
 });
 
 router.get('/addemployee',function(req, res) {
   res.render('addemployee');
+});
+
+router.get('/editemployee', function(req, res) {
+  res.render('editemployee');
 });
 
 router.post('/schedule', function(req, res) {
