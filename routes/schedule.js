d8f3e161e892fa541a797181ced13306d132f64b
Jeff Irwin <jeff.irwin@live.com>
2017-08-07 17:47:26 -0500

    fixed bug

--- routes/schedule.js
+++ routes/schedule.js
@@ -2,18 +2,19 @@ const express = require('express')
 const router = require('express').Router();
 const expressValidator = require('express-validator');
 const bodyParser = require('body-parser');
+const inventoryModel = require('../models/data');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 
-const inventoryModel = require('../models/data');
 
 router.get('/schedule', function(req, res) {
   inventoryModel.getEmployeeSchedule(function(err, data) {
     res.render('employeeschedule', { schedule: data });
   });
 });
+
 //Attempted to assign validation to this router. No errors, but not running at the same time.
 router.get('/addemployee',function(req, res) {
   req.check('name', 'Name can not be empty').notEmpty();
